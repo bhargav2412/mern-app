aa03f367638c9ca75773577a0105be5d63b8031f
bhargav2412 <bhargavgohel10@gmail.com>
2024-08-01 16:20:36 +0530

    completed profile upload

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "mern-blog-b7c53.firebaseapp.com",
-    projectId: "mern-blog-b7c53",
-    storageBucket: "mern-blog-b7c53.appspot.com",
-    messagingSenderId: "115969596325",
-    appId: "1:115969596325:web:11615d0c73f53b3249664c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-b7c53.firebaseapp.com",
+  projectId: "mern-blog-b7c53",
+  storageBucket: "mern-blog-b7c53.appspot.com",
+  messagingSenderId: "115969596325",
+  appId: "1:115969596325:web:11615d0c73f53b3249664c",
 };
 
 // Initialize Firebase
